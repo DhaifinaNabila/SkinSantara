094e0b43930a59b643cc652cc71a97d9da381254
DhaifinaNabila <dhaifinazyx@gmail.com>
2025-06-27 20:58:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+
+function ubahUkuranTeks(ukuran) {
+  let body = document.body;
+
+  switch (ukuran) {
+    case 'besar':
+      body.style.fontSize = '1.15em';
+      break;
+    case 'normal':
+      body.style.fontSize = '1em';
+      break;
+    case 'kecil':
+      body.style.fontSize = '0.9em';
+      break;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollBtn = document.getElementById("scrollTopBtn");
+
+  // Tampilkan tombol saat scroll ke bawah
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      scrollBtn.style.display = "block";
+    } else {
+      scrollBtn.style.display = "none";
+    }
+  });
+
+  // Scroll ke atas saat tombol diklik
+  scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+});
+
 // Validasi Form Upload
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('formUpload');
